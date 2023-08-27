7be9ff9cb10a526ac223a662bed8cd79bd1e46d7
Gabriel Coelho <gabrielbr100@outlook.com>
2023-08-27 12:49:13 -0300

    alteraçao

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Login from "./Pages/Login";
 import Home from "./Pages/Home";
 import CadProdutos from "./Pages/CadProdutos/TelaCadProdutos";
 import TelaCadPessoa from "./Pages/CadPessoas/CadPessoa";
 import CadServicos from "./Pages/CadServicos/TelaCadServicos";
 import CadCategoria from "./Pages/CadCategoria/TelaCadCategoria";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Login />} />
+          {/* <Route path="/" element={<Login />} /> */}
           <Route path="/Home" element={<Home />} />
           <Route path="/CadastroProduto" element={<CadProdutos />} />
           <Route path="/CadastroPessoas" element={<TelaCadPessoa />} />
